2d02190efcd4f1f7ee7dc50fe4047c693891929a
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-12 00:02:07 +0900

    Initial Gradle Project

---
+++ src/main/kotlin/com/masahirosaito/spigot/homes/Homes.kt
@@ -0,0 +1,15 @@
+package com.masahirosaito.spigot.homes
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Homes : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
