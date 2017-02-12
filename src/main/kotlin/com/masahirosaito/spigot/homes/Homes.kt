dc2619ba7002afd045a4bc2e071208e5bfb06b38
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-12 10:18:58 +0900

    [Add] Messenger インスタンス

--- src/main/kotlin/com/masahirosaito/spigot/homes/Homes.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/Homes.kt
@@ -1,12 +1,15 @@
 package com.masahirosaito.spigot.homes
 
 import com.masahirosaito.spigot.homes.commands.HomeCommand
+import com.masahirosaito.spigot.mscore.Messenger
 import org.bukkit.plugin.java.JavaPlugin
 
 class Homes : JavaPlugin() {
+    lateinit var messenger: Messenger
     lateinit var homeManager: HomeManager
 
     override fun onEnable() {
+        messenger = Messenger(this, false)
         homeManager = HomeManager(this)
 
         getCommand("home").executor = HomeCommand(this)
