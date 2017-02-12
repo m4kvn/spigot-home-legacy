1d934cbc6bb76dd38af995a557dd9f7d29ad6495
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-12 11:12:24 +0900

    [Add] Configs インスタンス

--- src/main/kotlin/com/masahirosaito/spigot/homes/Homes.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/Homes.kt
@@ -2,17 +2,23 @@ package com.masahirosaito.spigot.homes
 
 import com.masahirosaito.spigot.homes.commands.HomeCommand
 import com.masahirosaito.spigot.mscore.Messenger
+import com.masahirosaito.spigot.mscore.utils.load
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class Homes : JavaPlugin() {
+    lateinit var configs: Configs
     lateinit var messenger: Messenger
     lateinit var homeManager: HomeManager
 
     override fun onEnable() {
-        messenger = Messenger(this, true)
+        configs = Configs.fromFile(File(dataFolder, "configs.json").load())
+        messenger = Messenger(this, configs.onDebug)
         homeManager = HomeManager(this)
 
         getCommand("home").executor = HomeCommand(this)
+
+        messenger.debug("[設定確認] $configs")
     }
 
     override fun onDisable() {
