455122f9c20fd6cf4d2d627b726d9d05a74d0c2f
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-20 21:45:34 +0900

    [Edit] HomeCommand を新しい処理に修正

--- src/main/kotlin/com/masahirosaito/spigot/homes/commands/HomeCommand.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/commands/HomeCommand.kt
@@ -1,42 +1,48 @@
 package com.masahirosaito.spigot.homes.commands
 
 import com.masahirosaito.spigot.homes.Homes
-import com.masahirosaito.spigot.homes.commands.subcommands.HomeSetCommand
+import com.masahirosaito.spigot.homes.commands.subcommands.SetCommand
+import com.masahirosaito.spigot.homes.commands.subcommands.SubCommand
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
-class HomeCommand(plugin: Homes) : CommandExecutor {
-    val messenger = plugin.messenger
-    val homeManager = plugin.homeManager
-    val subCommands = mapOf(
-            "set" to HomeSetCommand(plugin)
-    )
+class HomeCommand(val plugin: Homes) : CommandExecutor {
+    val permission = ""
+    val subCommands = listOf<SubCommand>(SetCommand(plugin))
 
-    override fun onCommand(sender: CommandSender?, command: Command?, label: String?, args: Array<out String>?): Boolean {
+    override fun onCommand(sender: CommandSender?, command: Command?,
+                           label: String?, args: Array<out String>?): Boolean {
 
         if (sender !is Player) return true
 
+//        if (!sender.hasPermission(permission)) return sendPermissionMsg(sender, permission)
+
         if (args == null || args.isEmpty()) return execute(sender)
 
-        if (subCommands.containsKey(args[0])) {
-            return subCommands[args[0]]?.execute(sender)!!
+        subCommands.find { it.name == args[0] }?.let {
+            if (it.hasPermission(sender)) it.execute(sender, args.drop(1))
+            else return sendPermissionMsg(sender, it.permission)
         }
 
         return true
     }
 
-    fun execute(player: Player): Boolean {
-        val playerHomes = homeManager.getPlayerHomes(player.uniqueId)
-        val defaultHome = playerHomes.defaultHome
-
-        if (defaultHome.locationData == null) {
-            messenger.send(player, "ホームが設定されていません！")
-        } else {
-            player.teleport(defaultHome.locationData?.toLocation())
+    fun execute(player: Player) : Boolean {
+        plugin.homedata.playerHomes[player.uniqueId]?.let { playerHome ->
+            playerHome.defaultHome?.let { player.teleport(it.toLocation()) }
         }
+        return true
+    }
 
+    fun sendPermissionMsg(player: Player, permission: String): Boolean {
+        plugin.messenger.send(player, buildString {
+            append(ChatColor.RED)
+            append("You don't have permission <$permission>")
+            append(ChatColor.RESET)
+        })
         return true
     }
 }
