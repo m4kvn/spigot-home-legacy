4f1c42b1d8eb24fdeeed735ff6ddae6aa3bdb687
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-20 21:44:05 +0900

    [Add] Messenger

---
+++ src/main/kotlin/com/masahirosaito/spigot/homes/Messenger.kt
@@ -0,0 +1,19 @@
+package com.masahirosaito.spigot.homes
+
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+
+class Messenger(val plugin: JavaPlugin, val onDebug: Boolean) {
+
+    fun prefix(obj: Any) = "[${plugin.name}] $obj"
+
+    fun log(obj: Any) = Bukkit.getServer().consoleSender.sendMessage(prefix(obj))
+
+    fun debug(obj: Any) {
+        if (onDebug) log("${ChatColor.AQUA}[DEBUG]${ChatColor.RESET} $obj")
+    }
+
+    fun send(player: Player, obj: Any) = player.sendMessage(prefix(obj))
+}
