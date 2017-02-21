9e0a03c670f3c04c6383205e5da774eac13c3e68
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-22 03:54:43 +0900

    [Edit] set permissions in plugin.yml

--- src/main/kotlin/com/masahirosaito/spigot/homes/Permission.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/Permission.kt
@@ -1,7 +1,7 @@
 package com.masahirosaito.spigot.homes
 
 object Permission {
-    val home_command = "home.command"
+    val home_command = "homes.command"
     val home_command_name = home_command + ".name"
     val home_command_player = home_command + ".player"
     val home_command_player_name = home_command_player + ".name"
