820c12a1be38180e4db38ba94ef445201892172f
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-21 23:19:53 +0900

    [Edit] 不要インポート削除

--- src/main/kotlin/com/masahirosaito/spigot/homes/homedata/PlayerHome.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/homedata/PlayerHome.kt
@@ -1,7 +1,5 @@
 package com.masahirosaito.spigot.homes.homedata
 
-import com.masahirosaito.spigot.homes.homedata.LocationData
-
 data class PlayerHome(
         var defaultHome: LocationData? = null,
         val namedHomes: MutableMap<String, LocationData> = mutableMapOf()
