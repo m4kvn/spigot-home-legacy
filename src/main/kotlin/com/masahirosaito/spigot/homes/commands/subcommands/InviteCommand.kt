3118302179de978ca533dc02d129ecebceaab3c3
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-28 02:03:03 +0900

    [Fix] the spelling of invite command usage

--- src/main/kotlin/com/masahirosaito/spigot/homes/commands/subcommands/InviteCommand.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/commands/subcommands/InviteCommand.kt
@@ -21,7 +21,7 @@ class InviteCommand(override val plugin: Homes) : SubCommand {
     override fun description(): String = "Invite the other player to your home"
 
     override fun usages(): List<Pair<String, String>> = listOf(
-            "/home invite" to "Accept an invitation",
+            "/home invite" to "Accept the invitation",
             "/home invite <player_name>" to "Invite to your default home",
             "/home invite <player_name> <home_name>" to "Invite to your named home"
     )
