43021b64515ec6b13ac297ea5acdbc4b943305c7
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-02 01:15:19 +0900

    Add Util.java for TestInstanceCreator

---
+++ src/test/java/com/masahirosaito/spigot/homes/utils/Util.java
@@ -0,0 +1,47 @@
+package com.masahirosaito.spigot.homes.utils;
+
+import java.util.logging.*;
+
+public class Util {
+    private Util() {}
+
+    public static final Logger logger = Logger.getLogger("Homes-Test");
+
+    static {
+        logger.setUseParentHandlers(false);
+
+        Handler handler = new ConsoleHandler();
+        handler.setFormatter(new HomesTestLogFormatter());
+        Handler[] handlers = logger.getHandlers();
+
+        for (Handler h : handlers) logger.removeHandler(h);
+
+        logger.addHandler(handler);
+    }
+
+    public static void log(Throwable t) {
+        log(Level.WARNING, t.getLocalizedMessage(), t);
+    }
+
+    public static void log(Level level, Throwable t) {
+        log(level, t.getLocalizedMessage(), t);
+    }
+
+    public static void log(String message, Throwable t) {
+        log(Level.WARNING, message, t);
+    }
+
+    public static void log(Level level, String message, Throwable t) {
+        LogRecord record = new LogRecord(level, message);
+        record.setThrown(t);
+        logger.log(record);
+    }
+
+    public static void log(String message) {
+        log(Level.INFO, message);
+    }
+
+    public static void log(Level level, String message) {
+        logger.log(level, message);
+    }
+}
