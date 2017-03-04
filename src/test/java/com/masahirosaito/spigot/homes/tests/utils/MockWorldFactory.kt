c0c77d975ea81eff0b6e68185a92e24edcb8d6fa
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-04 14:33:00 +0900

    Update MockWorldFactory
    teardown時に world が初期化されていなかったので除去

--- src/test/java/com/masahirosaito/spigot/homes/tests/utils/MockWorldFactory.kt
+++ src/test/java/com/masahirosaito/spigot/homes/tests/utils/MockWorldFactory.kt
@@ -7,19 +7,19 @@ import java.util.*
 
 object MockWorldFactory {
     val worlds: MutableMap<UUID, World> = mutableMapOf()
-    val world: World = MockWorldFactory.makeNewMockWorld().apply { MockWorldFactory.worlds.put(uid, this) }
 
-    fun makeNewMockWorld(): World {
+    fun makeNewMockWorld(name: String): World {
         return PowerMockito.mock(World::class.java).apply {
             val uuid = UUID.randomUUID()
             PowerMockito.`when`(uid).thenReturn(uuid)
+            PowerMockito.`when`(getName()).thenReturn(name)
             MockWorldFactory.register(this)
         }
     }
 
     fun makeRandomLocation(): Location {
         return Random().run {
-            Location(world, nextDouble(), nextDouble(), nextDouble(), nextFloat(), nextFloat())
+            Location(makeNewMockWorld("world"), nextDouble(), nextDouble(), nextDouble(), nextFloat(), nextFloat())
         }
     }
 
