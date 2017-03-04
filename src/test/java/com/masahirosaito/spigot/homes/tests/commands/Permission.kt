b31013307174a18a695c1bd0171c8dc6454515fd
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-04 14:33:24 +0900

    Update Permission
    Setコマンド用の権限を追加

--- src/test/java/com/masahirosaito/spigot/homes/tests/commands/Permission.kt
+++ src/test/java/com/masahirosaito/spigot/homes/tests/commands/Permission.kt
@@ -1,8 +1,12 @@
 package com.masahirosaito.spigot.homes.tests.commands
 
 enum class Permission(val permission: String) {
+
     HOME_DEFAULT("homes.command"),
     HOME_NAME("homes.command.name"),
     HOME_PLAYER("homes.command.player"),
-    HOME_PLAYER_NAME("homes.command.player.name")
+    HOME_PLAYER_NAME("homes.command.player.name"),
+
+    HOME_SET("homes.command.set"),
+    HOME_SET_NAME("homes.command.set.name")
 }
