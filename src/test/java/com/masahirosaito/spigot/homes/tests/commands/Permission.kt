9fe3bbf767b6634a60bb162fe8d34cb692cb94f6
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-04 13:53:28 +0900

    Create Permission

---
+++ src/test/java/com/masahirosaito/spigot/homes/tests/commands/Permission.kt
@@ -0,0 +1,8 @@
+package com.masahirosaito.spigot.homes.tests.commands
+
+enum class Permission(val permission: String) {
+    HOME_DEFAULT("homes.command"),
+    HOME_NAME("homes.command.name"),
+    HOME_PLAYER("homes.command.player"),
+    HOME_PLAYER_NAME("homes.command.player.name")
+}
