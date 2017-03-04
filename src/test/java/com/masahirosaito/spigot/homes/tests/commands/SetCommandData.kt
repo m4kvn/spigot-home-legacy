2caa406ac6e49175f4061d16a6240c162d3a2065
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-04 13:53:39 +0900

    Create SetCommandData

---
+++ src/test/java/com/masahirosaito/spigot/homes/tests/commands/SetCommandData.kt
@@ -0,0 +1,15 @@
+package com.masahirosaito.spigot.homes.tests.commands
+
+object SetCommandData : CommandData {
+
+    override fun name(): String = "set"
+
+    override fun usages(): List<Pair<String, String>> = listOf(
+            "/home set" to "Set your location to your default home",
+            "/home set <home_name>" to "Set your location to your named home"
+    )
+
+    fun msgSuccessSetDefaultHome() = prefix("Successfully set as default home")
+
+    fun msgSuccessSetNamedHome(name: String) = prefix("Successfully set as home named <$name>")
+}
