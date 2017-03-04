c3278761564a7774cfd9ae2753ac14d82a202847
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-04 13:56:47 +0900

    Update MockPlayerFactory

--- src/test/java/com/masahirosaito/spigot/homes/tests/utils/MockPlayerFactory.kt
+++ src/test/java/com/masahirosaito/spigot/homes/tests/utils/MockPlayerFactory.kt
@@ -1,16 +1,13 @@
-package com.masahirosaito.spigot.homes.utils
+package com.masahirosaito.spigot.homes.tests.utils
 
 import org.bukkit.Location
 import org.bukkit.OfflinePlayer
 import org.bukkit.Server
-import org.bukkit.World
 import org.bukkit.entity.Player
-import org.mockito.Matchers
 import org.mockito.Matchers.any
 import org.mockito.Matchers.anyString
 import org.powermock.api.mockito.PowerMockito
 import org.powermock.api.mockito.PowerMockito.doAnswer
-import org.powermock.api.mockito.PowerMockito.mock
 import java.util.*
 
 object MockPlayerFactory {
@@ -24,29 +21,32 @@ object MockPlayerFactory {
             val uuid = UUID.randomUUID()
             PowerMockito.`when`(server).thenReturn(mockServer)
             PowerMockito.`when`(name).thenReturn(playerName)
-            PowerMockito.`when`(hasPermission(Matchers.anyString())).thenReturn(true)
             PowerMockito.`when`(uniqueId).thenReturn(uuid)
-            PowerMockito.`when`(location).then { locations[uniqueId] }
+            PowerMockito.`when`(location).then { MockPlayerFactory.locations[uniqueId] }
+            PowerMockito.`when`(hasPermission(anyString())).thenAnswer { invocation ->
+                MockPlayerFactory.permissions[uniqueId]?.contains(invocation.getArgumentAt(0, String::class.java))
+            }
             doAnswer({ invocation ->
-                locations.put(uniqueId, invocation.getArgumentAt(0, Location::class.java)); true
+                MockPlayerFactory.locations.put(uniqueId, invocation.getArgumentAt(0, Location::class.java)); true
             }).`when`(this).teleport(any(Location::class.java))
             doAnswer({ invocation ->
                 server.logger.info(invocation.getArgumentAt(0, String::class.java))
             }).`when`(this).sendMessage(anyString())
-            register(this)
+            MockPlayerFactory.register(this)
         }
     }
 
     private fun register(player: Player) {
-        players.put(player.uniqueId, player)
-        locations.put(player.uniqueId, Location(mock(World::class.java), 0.0, 0.0, 0.0))
-        permissions.put(player.uniqueId, mutableListOf())
-        offlinePlayers.put(player.uniqueId, player)
+        MockPlayerFactory.players.put(player.uniqueId, player)
+        MockPlayerFactory.locations.put(player.uniqueId, MockWorldFactory.makeRandomLocation())
+        MockPlayerFactory.permissions.put(player.uniqueId, mutableListOf())
+        MockPlayerFactory.offlinePlayers.put(player.uniqueId, player)
     }
 
     fun clear() {
-        players.clear()
-        locations.clear()
-        permissions.clear()
+        MockPlayerFactory.players.clear()
+        MockPlayerFactory.locations.clear()
+        MockPlayerFactory.permissions.clear()
+        MockPlayerFactory.offlinePlayers.clear()
     }
 }
