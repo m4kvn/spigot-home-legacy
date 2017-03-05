67fde3be41c440a69943281fa7d22addc5185edc
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-05 11:15:39 +0900

    Change Utils set function

--- src/test/java/com/masahirosaito/spigot/homes/tests/utils/Utils.kt
+++ src/test/java/com/masahirosaito/spigot/homes/tests/utils/Utils.kt
@@ -4,8 +4,8 @@ import com.masahirosaito.spigot.homes.tests.Permission
 import org.bukkit.Location
 import org.bukkit.entity.Player
 
-fun Player.set(permission: Permission) {
-    MockPlayerFactory.permissions[uniqueId]?.add(permission.permission)
+fun Player.set(vararg permissions: Permission) {
+    permissions.forEach { MockPlayerFactory.permissions[uniqueId]?.add(it.permission) }
 }
 
 fun Player.setOps() {
