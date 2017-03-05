7fabe8328d955e7de21b5dbf660f4b2d821edcbf
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-05 19:44:20 +0900

    Update InviteCommandTest

--- src/test/java/com/masahirosaito/spigot/homes/tests/InviteCommandTest.kt
+++ src/test/java/com/masahirosaito/spigot/homes/tests/InviteCommandTest.kt
@@ -4,11 +4,11 @@ import com.masahirosaito.spigot.homes.Homes
 import com.masahirosaito.spigot.homes.tests.commands.DeleteCommandData
 import com.masahirosaito.spigot.homes.tests.commands.InviteCommandData
 import com.masahirosaito.spigot.homes.tests.commands.SetCommandData
+import com.masahirosaito.spigot.homes.tests.exceptions.CanNotFindDefaultHomeException
+import com.masahirosaito.spigot.homes.tests.exceptions.CanNotFindNamedHomeException
+import com.masahirosaito.spigot.homes.tests.exceptions.NotAllowedByConfigException
 import com.masahirosaito.spigot.homes.tests.exceptions.NotHavePermissionException
-import com.masahirosaito.spigot.homes.tests.utils.MockPlayerFactory
-import com.masahirosaito.spigot.homes.tests.utils.MockWorldFactory
-import com.masahirosaito.spigot.homes.tests.utils.TestInstanceCreator
-import com.masahirosaito.spigot.homes.tests.utils.set
+import com.masahirosaito.spigot.homes.tests.utils.*
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Server
@@ -25,6 +25,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.modules.junit4.PowerMockRunner
+import java.lang.Thread.sleep
 
 @RunWith(PowerMockRunner::class)
 @PrepareForTest(Homes::class, JavaPluginLoader::class, PluginDescriptionFile::class,
@@ -56,9 +57,6 @@ class InviteCommandTest {
         nepian.set(Permission.HOME_PLAYER, Permission.HOME_PLAYER_NAME)
         nepian.set(Permission.HOME_SET, Permission.HOME_SET_NAME)
 
-        minene.set(Permission.HOME_DEFAULT, Permission.HOME_NAME)
-        minene.set(Permission.HOME_PLAYER, Permission.HOME_PLAYER_NAME)
-
         nepian.teleport(MockWorldFactory.makeRandomLocation())
         defaultLocation = nepian.location
         command.onCommand(nepian, pluginCommand, "home", arrayOf("set"))
@@ -68,6 +66,8 @@ class InviteCommandTest {
         namedLocation = nepian.location
         command.onCommand(nepian, pluginCommand, "home", arrayOf("set", "home1"))
         Assert.assertEquals(SetCommandData.msgSuccessSetNamedHome("home1"), logs.last())
+
+        minene.teleport(MockWorldFactory.makeRandomLocation())
     }
 
     @After
@@ -76,8 +76,108 @@ class InviteCommandTest {
     }
 
     @Test
-    fun コマンドの親権限を持っていない場合() {
+    fun コマンドの親権限を持っていない場合は招待許可以外は行えない() {
         command.onCommand(nepian, pluginCommand, "home", arrayOf("invite"))
+        Assert.assertEquals(InviteCommandData.msgNoReceivedInvitation(), logs.last())
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene"))
         Assert.assertEquals(InviteCommandData.msg(NotHavePermissionException(Permission.HOME_INVITE)), logs.last())
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene", "home1"))
+        Assert.assertEquals(InviteCommandData.msg(NotHavePermissionException(Permission.HOME_INVITE)), logs.last())
+
+        nepian.set(Permission.HOME_INVITE_NAME)
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene"))
+        Assert.assertEquals(InviteCommandData.msg(NotHavePermissionException(Permission.HOME_INVITE)), logs.last())
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene", "home1"))
+        Assert.assertEquals(InviteCommandData.msg(NotHavePermissionException(Permission.HOME_INVITE)), logs.last())
+    }
+
+    @Test
+    fun 権限がなくても招待を受けることができる() {
+        nepian.set(Permission.HOME_INVITE, Permission.HOME_INVITE_NAME)
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene"))
+        Assert.assertEquals(InviteCommandData.msgReceivedInvitationFrom(nepian), logs[logs.lastIndex - 1])
+        Assert.assertEquals(InviteCommandData.msgInvited(minene), logs.last())
+
+        command.onCommand(minene, pluginCommand, "home", arrayOf("invite"))
+        Assert.assertEquals(InviteCommandData.msgAcceptedInvitationFrom(nepian), logs[logs.lastIndex-1])
+        Assert.assertEquals(InviteCommandData.msgAcceptedInvitation(minene), logs.last())
+        Assert.assertEquals(defaultLocation, minene.location)
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene", "home1"))
+        Assert.assertEquals(InviteCommandData.msgReceivedInvitationFrom(nepian, "home1"), logs[logs.lastIndex - 1])
+        Assert.assertEquals(InviteCommandData.msgInvited(minene, "home1"), logs.last())
+
+        command.onCommand(minene, pluginCommand, "home", arrayOf("invite"))
+        Assert.assertEquals(InviteCommandData.msgAcceptedInvitationFrom(nepian), logs[logs.lastIndex-1])
+        Assert.assertEquals(InviteCommandData.msgAcceptedInvitation(minene), logs.last())
+        Assert.assertEquals(namedLocation, minene.location)
+    }
+
+    @Test
+    fun 設定がオフの場合は招待機能を利用できない() {
+        homes.configs.copy(onInvite = false).apply {
+            save(TestInstanceCreator.configFile)
+            homes.onDisable()
+            homes.onEnable()
+            Assert.assertEquals(this, homes.configs)
+        }
+        nepian.setOps()
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene"))
+        Assert.assertEquals(InviteCommandData.msg(NotAllowedByConfigException()), logs.last())
+
+        command.onCommand(minene, pluginCommand, "home", arrayOf("invite"))
+        Assert.assertEquals(InviteCommandData.msg(NotAllowedByConfigException()), logs.last())
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene", "home1"))
+        Assert.assertEquals(InviteCommandData.msg(NotAllowedByConfigException()), logs.last())
+
+        command.onCommand(minene, pluginCommand, "home", arrayOf("invite"))
+        Assert.assertEquals(InviteCommandData.msg(NotAllowedByConfigException()), logs.last())
+    }
+
+    @Test
+    fun ホームが設定されていない場合は招待できない() {
+        nepian.setOps()
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("delete"))
+        Assert.assertEquals(DeleteCommandData.getResultMessage(null), logs.last())
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene"))
+        Assert.assertEquals(InviteCommandData.msg(CanNotFindDefaultHomeException(nepian)), logs.last())
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("delete", "home1"))
+        Assert.assertEquals(DeleteCommandData.getResultMessage("home1"), logs.last())
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene", "home1"))
+        Assert.assertEquals(InviteCommandData.msg(CanNotFindNamedHomeException(nepian, "home1")), logs.last())
+    }
+
+    @Test
+    fun 三十秒間招待を受けなかった場合は自動的に拒否される() {
+        nepian.setOps()
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene"))
+        Assert.assertEquals(InviteCommandData.msgReceivedInvitationFrom(nepian), logs[logs.lastIndex - 1])
+        Assert.assertEquals(InviteCommandData.msgInvited(minene), logs.last())
+
+        sleep(31000)
+
+        Assert.assertEquals(InviteCommandData.msgCanceledInvitationFrom(nepian), logs[logs.lastIndex - 1])
+        Assert.assertEquals(InviteCommandData.msgCanceledInvitation(minene), logs.last())
+
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("invite", "Minene", "home1"))
+        Assert.assertEquals(InviteCommandData.msgReceivedInvitationFrom(nepian, "home1"), logs[logs.lastIndex - 1])
+        Assert.assertEquals(InviteCommandData.msgInvited(minene, "home1"), logs.last())
+
+        sleep(31000)
+
+        Assert.assertEquals(InviteCommandData.msgCanceledInvitationFrom(nepian), logs[logs.lastIndex - 1])
+        Assert.assertEquals(InviteCommandData.msgCanceledInvitation(minene), logs.last())
     }
 }
