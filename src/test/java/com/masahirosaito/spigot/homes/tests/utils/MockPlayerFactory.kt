cb4e89e336f3f79976234848de9b41b482235610
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-05 19:38:35 +0900

    Refactor MockPlayerFactory

--- src/test/java/com/masahirosaito/spigot/homes/tests/utils/MockPlayerFactory.kt
+++ src/test/java/com/masahirosaito/spigot/homes/tests/utils/MockPlayerFactory.kt
@@ -5,10 +5,11 @@ import org.bukkit.Location
 import org.bukkit.OfflinePlayer
 import org.bukkit.Server
 import org.bukkit.entity.Player
+import org.bukkit.metadata.MetadataValue
+import org.bukkit.plugin.java.JavaPlugin
 import org.mockito.Matchers.any
 import org.mockito.Matchers.anyString
 import org.powermock.api.mockito.PowerMockito
-import org.powermock.api.mockito.PowerMockito.doAnswer
 import java.util.*
 
 object MockPlayerFactory {
@@ -17,6 +18,7 @@ object MockPlayerFactory {
     val permissions: MutableMap<UUID, MutableList<String>> = mutableMapOf()
     val offlinePlayers: MutableMap<UUID, OfflinePlayer> = mutableMapOf()
     val ops: MutableList<UUID> = mutableListOf()
+    val metadatas: MutableMap<UUID, MutableMap<String, MutableList<MetadataValue>>> = mutableMapOf()
 
     fun makeNewMockPlayer(playerName: String, mockServer: Server): Player {
         return PowerMockito.mock(Player::class.java).apply {
@@ -25,16 +27,45 @@ object MockPlayerFactory {
             PowerMockito.`when`(name).thenReturn(playerName)
             PowerMockito.`when`(uniqueId).thenReturn(uuid)
             PowerMockito.`when`(location).then { locations[uniqueId] }
+
+            /* TODO: hasPermission(permission: String): Boolean */
             PowerMockito.`when`(hasPermission(anyString())).thenAnswer { invocation ->
                 if (isOps()) true
                 else permissions[uniqueId]?.contains(invocation.getArgumentAt(0, String::class.java))
             }
-            doAnswer({ invocation ->
+
+            /* TODO: getMetadata(metadataKey: String): List<MetadataValue> */
+            PowerMockito.`when`(getMetadata(anyString())).thenAnswer { invocation ->
+                metadatas[uniqueId]?.get(invocation.getArgumentAt(0, String::class.java))
+            }
+
+            /* TODO: setMetadata(metadataKey: String, newMetadataValue: MetadataValue): Unit */
+            PowerMockito.`when`(setMetadata(anyString(), any(MetadataValue::class.java))).thenAnswer { invocation ->
+                metadatas[uniqueId]?.put(
+                        invocation.getArgumentAt(0, String::class.java),
+                        mutableListOf(invocation.getArgumentAt(1, MetadataValue::class.java)))
+            }
+
+            /* TODO: hasMetadata(metadataKey: String): Boolean */
+            PowerMockito.`when`(hasMetadata(anyString())).thenAnswer { invocation ->
+                metadatas[uniqueId]?.contains(invocation.getArgumentAt(0, String::class.java))
+            }
+
+            /* TODO: removeMetadata(metadataKey: String, plugin: JavaPlugin): Unit */
+            PowerMockito.`when`(removeMetadata(anyString(), any(JavaPlugin::class.java))).thenAnswer { invocation ->
+                metadatas[uniqueId]?.remove(invocation.getArgumentAt(0, String::class.java))
+            }
+
+            /* TODO: teleport(location: Location): Boolean */
+            PowerMockito.`when`(teleport(any(Location::class.java))).thenAnswer { invocation ->
                 locations.put(uniqueId, invocation.getArgumentAt(0, Location::class.java)); true
-            }).`when`(this).teleport(any(Location::class.java))
-            doAnswer({ invocation ->
+            }
+
+            /* TODO: sendMessage(msg: String): Unit */
+            PowerMockito.`when`(sendMessage(anyString())).thenAnswer { invocation ->
                 server.logger.info(invocation.getArgumentAt(0, String::class.java))
-            }).`when`(this).sendMessage(anyString())
+            }
+
             register(this)
         }
     }
@@ -44,6 +75,7 @@ object MockPlayerFactory {
         locations.put(player.uniqueId, makeRandomLocation())
         permissions.put(player.uniqueId, mutableListOf())
         offlinePlayers.put(player.uniqueId, player)
+        metadatas.put(player.uniqueId, mutableMapOf())
     }
 
     fun clear() {
@@ -52,5 +84,6 @@ object MockPlayerFactory {
         permissions.clear()
         offlinePlayers.clear()
         ops.clear()
+        metadatas.clear()
     }
 }
