cd1ae0a0a9366c9003ca35ae293e83c43bc6734e
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-05 09:41:06 +0900

    Add some player functions to Utils

--- src/test/java/com/masahirosaito/spigot/homes/tests/utils/Utils.kt
+++ src/test/java/com/masahirosaito/spigot/homes/tests/utils/Utils.kt
@@ -1,8 +1,15 @@
 package com.masahirosaito.spigot.homes.tests.utils
 
 import com.masahirosaito.spigot.homes.tests.commands.Permission
+import org.bukkit.Location
 import org.bukkit.entity.Player
 
 fun Player.set(permission: Permission) {
     MockPlayerFactory.permissions[uniqueId]?.add(permission.permission)
 }
+
+fun Player.setOps() {
+    MockPlayerFactory.ops.add(uniqueId)
+}
+
+fun Player.isOps() = MockPlayerFactory.ops.contains(uniqueId)
