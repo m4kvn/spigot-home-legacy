0168fc3fd83fa55362b9512016aaa7316834d732
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-10 16:57:03 +0900

    remove checkPrivate method from HomeData

--- src/main/kotlin/com/masahirosaito/spigot/homes/homedata/HomeData.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/homedata/HomeData.kt
@@ -1,7 +1,5 @@
 package com.masahirosaito.spigot.homes.homedata
 
-import com.masahirosaito.spigot.homes.exceptions.PlayerHomeIsPrivateException
-import org.bukkit.OfflinePlayer
 import java.util.*
 
 data class HomeData(
@@ -11,8 +9,4 @@ data class HomeData(
         var isPrivate: Boolean = false
 ) {
     fun location() = locationData.toLocation()
-
-    fun checkPrivate(offlinePlayer: OfflinePlayer, homeName: String? = null) = this.apply {
-        if (isPrivate) throw PlayerHomeIsPrivateException(offlinePlayer, homeName)
-    }
 }
