63790ae2859473a4cb117114b3c729f394a0490e
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-10 16:30:18 +0900

    Create BaseCommand

---
+++ src/main/kotlin/com/masahirosaito/spigot/homes/commands/BaseCommand.kt
@@ -0,0 +1,44 @@
+package com.masahirosaito.spigot.homes.commands
+
+import com.masahirosaito.spigot.homes.Homes
+import com.masahirosaito.spigot.homes.exceptions.InValidCommandSenderException
+import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
+import org.bukkit.entity.Player
+
+interface BaseCommand {
+    val plugin: Homes
+    val name: String
+    val description: String
+    val usage: CommandUsage
+    val commands: List<BaseCommand>
+
+    fun configs(): List<Boolean>
+
+    fun isValidArgs(args: List<String>): Boolean
+
+    fun send(sender: CommandSender, message: String) {
+        if (!message.isNullOrBlank()) plugin.messenger.send(sender, message)
+    }
+
+    fun checkConfig() {
+        if (configs().contains(false)) throw Exception("Not allowed by the configuration of this server")
+    }
+
+    fun checkArgs(args: List<String>) {
+        if (!isValidArgs(args)) throw Exception("The argument is incorrect\n$usage")
+    }
+
+    fun executeCommand(sender: CommandSender,  args: List<String>) {
+        val cmd = findCommand(args).apply { println("$this.onCommand") }
+        when {
+            cmd is PlayerCommand && sender is Player -> cmd.onCommand(sender, args)
+            cmd is ConsoleCommand && sender is ConsoleCommandSender -> cmd.onCommand(sender, args)
+            else -> throw InValidCommandSenderException()
+        }
+    }
+
+    private fun findCommand(args: List<String>): BaseCommand {
+        return commands.find { it.isValidArgs(args) } ?: this
+    }
+}
