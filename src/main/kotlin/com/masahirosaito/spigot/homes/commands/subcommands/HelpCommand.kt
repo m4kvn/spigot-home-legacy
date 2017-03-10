bc245bf0e2e7154a3390cbc3fbcc9bb456ea89d3
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-10 19:24:30 +0900

    Update HelpCommand

--- src/main/kotlin/com/masahirosaito/spigot/homes/commands/subcommands/HelpCommand.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/commands/subcommands/HelpCommand.kt
@@ -2,17 +2,13 @@ package com.masahirosaito.spigot.homes.commands.subcommands
 
 import com.masahirosaito.spigot.homes.Homes
 import com.masahirosaito.spigot.homes.Permission
-import com.masahirosaito.spigot.homes.commands.BaseCommand
-import com.masahirosaito.spigot.homes.commands.CommandUsage
-import com.masahirosaito.spigot.homes.commands.PlayerCommand
-import com.masahirosaito.spigot.homes.commands.SubCommand
+import com.masahirosaito.spigot.homes.commands.*
 import com.masahirosaito.spigot.homes.exceptions.NoSuchCommandException
-import com.masahirosaito.spigot.homes.exceptions.NotHavePermissionException
-import com.masahirosaito.spigot.homes.findDefaultHome
 import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
-class HelpCommand(override val plugin: Homes) : PlayerCommand {
+class HelpCommand(val mainCommand: MainCommand) : PlayerCommand {
+    override val plugin: Homes = mainCommand.plugin
     override val name: String = "help"
     override val fee: Double = plugin.fee.HELP
     override val description: String = "Homes Help Command"
@@ -33,10 +29,20 @@ class HelpCommand(override val plugin: Homes) : PlayerCommand {
     override fun isValidArgs(args: List<String>): Boolean = args.isEmpty()
 
     override fun execute(player: Player, args: List<String>) {
-
+        send(player, msg())
     }
 
-    class HelpUsageCommand(helpCommand: HelpCommand) : SubCommand(helpCommand), PlayerCommand {
+    fun msg() = buildString {
+        append("${ChatColor.GOLD}Homes command list${ChatColor.RESET}\n")
+        append(ChatColor.LIGHT_PURPLE)
+        append("/home help <command_name> : Display the usage of command\n")
+        append(ChatColor.RESET)
+        mainCommand.subCommands.forEach {
+            append("  ${ChatColor.AQUA}${it.name}${ChatColor.RESET} : ${it.description}\n")
+        }
+    }
+
+    class HelpUsageCommand(val helpCommand: HelpCommand) : SubCommand(helpCommand), PlayerCommand {
         override val fee: Double = plugin.fee.HELP_USAGE
         override val permissions: List<String> = listOf(
                 Permission.home_command,
@@ -48,32 +54,9 @@ class HelpCommand(override val plugin: Homes) : PlayerCommand {
         override fun isValidArgs(args: List<String>): Boolean = args.size == 1
 
         override fun execute(player: Player, args: List<String>) {
-
+            helpCommand.mainCommand.subCommands.find { it.name == args[0] }?.let {
+                send(player, it.usage)
+            } ?: throw NoSuchCommandException(args[0])
         }
     }
-
-//    override fun isInValidArgs(args: List<String>): Boolean = args.size > 1
-//
-//    override fun execute(player: Player, args: List<String>) {
-//        when (args.size) {
-//            0 -> showCommands(player)
-//            1 -> showCommandUsage(player, args[0])
-//        }
-//    }
-//
-//    private fun showCommands(player: Player) {
-//        send(player, buildString {
-//            append("${ChatColor.GOLD}Homes command list${ChatColor.RESET}\n")
-//            append(ChatColor.LIGHT_PURPLE)
-//            append("/home help <command_name> : Display the usage of command\n")
-//            append(ChatColor.RESET)
-//            homeCommand.subCommands.forEach {
-//                append("  ${ChatColor.AQUA}${it.name()}${ChatColor.RESET} : ${it.description()}\n")
-//            }
-//        })
-//    }
-//
-//    private fun showCommandUsage(player: Player, name: String) {
-//        send(player, homeCommand.findSubCommand(name).usage())
-//    }
 }
