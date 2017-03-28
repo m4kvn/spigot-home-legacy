a030369bf89a8ee2225632dc9ea05240edd8eaea
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-28 12:16:34 +0900

    Create NMSManager v1.10_R1

---
+++ src/main/kotlin/com/masahirosaito/spigot/homes/nms/v1_10_R1/NMSManager_v1_10_R1.kt
@@ -0,0 +1,14 @@
+package com.masahirosaito.spigot.homes.nms.v1_10_R1
+
+import com.masahirosaito.spigot.homes.nms.NMSManager
+import org.bukkit.Location
+
+class NMSManager_v1_10_R1 : NMSManager {
+
+    override fun setUp() {
+    }
+
+    override fun spawnNMSArmorStand(loc: Location, name: String) {
+
+    }
+}
