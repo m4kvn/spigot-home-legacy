29acb520965a7f529b62bc58014de7fe97499207
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-04-03 13:42:55 +0900

    Create StringDatas

---
+++ src/main/kotlin/com/masahirosaito/spigot/homes/datas/strings/ErrorStringData.kt
@@ -0,0 +1,15 @@
+package com.masahirosaito.spigot.homes.datas.strings
+
+data class ErrorStringData(
+        val NO_COMMAND: String = "Command <[command-name]> does not exist",
+        val NO_PERMISSION: String = "You don't have permission <[permission-name]>",
+        val NO_VAULT: String = "Fee function stopped because Vault can not be found.",
+        val NO_ECONOMY: String = "Fee function stopped because the Economy plugin can not be found.",
+        val NO_DEFAULT_HOME: String = "[player-name]'s default home does not exist",
+        val NO_NAMED_HOME: String = "[player-name]'s home named <[home-name]> does not exist",
+        val HOME_LIMIT: String = "You can not set more homes (Limit: &r[home-limit-num]&c)",
+        val DEFAULT_HOME_IS_PRIVATE: String = "[player-name]'s default home is PRIVATE",
+        val NAMED_HOME_IS_PRIVATE: String = "[player-name]'s home named <[home-name]>} is PRIVATE",
+        val NO_RECEIVED_INVITATION: String = "You have not received an invitation",
+        val ALREADY_HAS_INVITATION: String = "[player-name] already has another invitation"
+)
---
+++ src/main/kotlin/com/masahirosaito/spigot/homes/datas/strings/commands/RemoveCommandStringData.kt
@@ -0,0 +1,6 @@
+package com.masahirosaito.spigot.homes.datas.strings.commands
+
+data class RemoveCommandStringData(
+        val REMOVE_DEFAULT_HOME: String = "b3Successfully delete your default home&r",
+        val REMOVE_NAMED_HOME: String = "&bSuccessfully delete your named home <&r[home-name]&b>&r"
+)
