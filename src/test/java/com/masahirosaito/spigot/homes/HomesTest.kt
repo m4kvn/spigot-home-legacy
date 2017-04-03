8c9e25de226a0b8b6e1ed32a2e8529c8630226dd
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-04-03 16:38:16 +0900

    Update Tests

--- src/test/java/com/masahirosaito/spigot/homes/HomesTest.kt
+++ src/test/java/com/masahirosaito/spigot/homes/HomesTest.kt
@@ -2,14 +2,29 @@ package com.masahirosaito.spigot.homes
 
 import com.masahirosaito.spigot.homes.commands.maincommands.homecommands.HomeCommand
 import com.masahirosaito.spigot.homes.datas.FeeData
+import com.masahirosaito.spigot.homes.strings.commands.DeleteCommandStrings.DELETE_DEFAULT_HOME
+import com.masahirosaito.spigot.homes.strings.commands.DeleteCommandStrings.DELETE_NAMED_HOME
+import com.masahirosaito.spigot.homes.strings.commands.PrivateCommandStrings.SET_DEFAULT_HOME_PRIVATE
+import com.masahirosaito.spigot.homes.strings.commands.PrivateCommandStrings.SET_DEFAULT_HOME_PUBLIC
+import com.masahirosaito.spigot.homes.strings.commands.PrivateCommandStrings.SET_NAMED_HOME_PRIVATE
+import com.masahirosaito.spigot.homes.strings.commands.PrivateCommandStrings.SET_NAMED_HOME_PUBLIC
+import com.masahirosaito.spigot.homes.strings.commands.SetCommandStrings.SET_DEFAULT_HOME
+import com.masahirosaito.spigot.homes.strings.commands.SetCommandStrings.SET_NAMED_HOME
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator
+import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.command
+import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.defaultLocation
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.feeFile
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.homes
+import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.minene
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.mockPluginManager
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.mockServicesManager
+import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.namedLocation
+import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.nepian
+import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.pluginCommand
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.pluginFolder
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.registeredServiceProvider
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.spyLogger
+import com.masahirosaito.spigot.homes.testutils.lastMsg
 import net.milkbowl.vault.economy.Economy
 import org.bukkit.Bukkit
 import org.bukkit.Location
@@ -89,24 +104,85 @@ class HomesTest {
         assertThat(homes.econ, `is`(notNullValue()))
     }
 
-    @Ignore
     @Test
-    fun 経済プラグインがない場合はコマンド料金機能の設定停止メッセージを表示する() {
+    fun ホームへ移動するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", null)
+        assertThat(nepian.location, `is`(defaultLocation))
+    }
 
-        "Fee function stopped because the Economy plugin can not be found.".apply {
+    @Test
+    fun 名前付きホームへ移動するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("home1"))
+        assertThat(nepian.location, `is`(namedLocation))
+    }
 
-            doReturn(null).`when`(mockServicesManager).getRegistration(Economy::class.java)
-            homes.onDisable()
-            homes.onEnable()
-            assertThat(spyLogger.logs.lastOrNull(), `is`(this))
-        }
+    @Test
+    fun 他人のホームへ移動するコマンドの実行ができる() {
+        command.onCommand(minene, pluginCommand, "home", arrayOf("-p", "Nepian"))
+        assertThat(minene.location, `is`(defaultLocation))
+    }
 
-        "Fee function stopped because Vault can not be found.".apply {
+    @Test
+    fun 他人の名前付きホームへ移動するコマンドの実行ができる() {
+        command.onCommand(minene, pluginCommand, "home", arrayOf("home1", "-p", "Nepian"))
+        assertThat(minene.location, `is`(namedLocation))
+    }
 
-            doReturn(null).`when`(mockPluginManager).getPlugin("Vault")
-            homes.onDisable()
-            homes.onEnable()
-            assertThat(spyLogger.logs.lastOrNull(), `is`(this))
-        }
+    @Test
+    fun ホームを設定するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("set"))
+        assertThat(nepian.lastMsg(), `is`(SET_DEFAULT_HOME()))
+    }
+
+    @Test
+    fun 名前付きホームを設定するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("set", "home1"))
+        assertThat(nepian.lastMsg(), `is`(SET_NAMED_HOME("home1")))
+    }
+
+    @Test
+    fun 設定されたホームを削除するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("delete"))
+        assertThat(nepian.lastMsg(), `is`(DELETE_DEFAULT_HOME()))
+    }
+
+    @Test
+    fun 設定された名前付きホームを削除するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("delete", "home1"))
+        assertThat(nepian.lastMsg(), `is`(DELETE_NAMED_HOME("home1")))
+    }
+
+    @Test
+    fun 設定されたホームをプライベート化するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("private", "on"))
+        assertThat(nepian.lastMsg(), `is`(SET_DEFAULT_HOME_PRIVATE()))
+    }
+
+    @Test
+    fun 設定された名前付きホームをプライベート化するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("private", "on", "home1"))
+        assertThat(nepian.lastMsg(), `is`(SET_NAMED_HOME_PRIVATE("home1")))
+    }
+
+    @Test
+    fun 設定されたホームをパブリック化するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("private", "off"))
+        assertThat(nepian.lastMsg(), `is`(SET_DEFAULT_HOME_PUBLIC()))
+    }
+
+    @Test
+    fun 設定された名前付きホームをパブリック化するコマンドの実行ができる() {
+        command.onCommand(nepian, pluginCommand, "home", arrayOf("private", "off", "home1"))
+        assertThat(nepian.lastMsg(), `is`(SET_NAMED_HOME_PUBLIC("home1")))
+    }
+
+    @Test
+    fun 設定されたホームの一覧を表示するコマンドの実行ができる() {
+        assertTrue(command.onCommand(nepian, pluginCommand, "home", arrayOf("list")))
+    }
+
+    @Test
+    fun 他の人の設定されたホームの一覧を表示するコマンドの実行ができる() {
+        assertTrue(command.onCommand(minene, pluginCommand, "home", arrayOf("list", "Nepian")))
     }
 }
