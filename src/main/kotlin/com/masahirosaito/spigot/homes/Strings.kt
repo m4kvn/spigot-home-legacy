f5e004c61daa1b21a58a47f6a2d0d7d4e9e9ee1f
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-04-03 13:45:37 +0900

    Update Strings Object

--- src/main/kotlin/com/masahirosaito/spigot/homes/Strings.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/Strings.kt
@@ -1,34 +1,26 @@
 package com.masahirosaito.spigot.homes
 
-import org.bukkit.ChatColor
+import com.masahirosaito.spigot.homes.strings.ErrorStrings
+import com.masahirosaito.spigot.homes.strings.HomeDisplayStrings
+import com.masahirosaito.spigot.homes.strings.commands.InviteCommandStrings
+import com.masahirosaito.spigot.homes.strings.commands.RemoveCommandStrings
 
 object Strings {
+    val PLAYER_NAME = "[player-name]"
+    val PERMISSION_NAME = "[permission-name]"
+    val HOME_NAME = "[home-name]"
+    val HOME_LIMIT_NUM = "[home-limit-num]"
+    val COMMAND_NAME = "[command-name]"
 
-    fun NO_DEFAULT_HOME(playerName: String)=
-            "$playerName's default home does not exist"
+    fun load(homes: Homes) {
+        "${homes.dataFolder}/languages/${Configs.language}".apply {
+            ErrorStrings.load(this)
+            HomeDisplayStrings.load(this)
 
-    fun NO_NAMED_HOME(playerName: String, homeName: String) =
-            "$playerName's home named <$homeName> does not exist"
-
-    fun LIMIT_HOME(limit: Int) =
-            "You can not set more homes (Limit: ${ChatColor.RESET}$limit${ChatColor.RED})"
-
-    fun HOME_DISPLAY_NAME(playerName: String, homeName: String? = null) =
-            "$playerName's\n${if (homeName == null) "default home" else "home named\n<$homeName>"}"
-
-    fun PRIVATE_HOME(playerName: String, homeName: String? = null) =
-            "$playerName's ${if (homeName == null) "default home" else "home named <$homeName>"} is PRIVATE"
-
-    fun REMOVE_DEFAULT_HOME() =
-            "${ChatColor.AQUA}Successfully delete your default home${ChatColor.RESET}"
-
-    fun REMOVE_NAMED_HOME(homeName: String) =
-            "${ChatColor.AQUA}Successfully delete your named home" +
-            " <${ChatColor.RESET}$homeName${ChatColor.AQUA}>${ChatColor.RESET}"
-
-    fun NO_COMMAND(commandName: String) =
-            "Command <$commandName> does not exist"
-
-    fun NO_PERMISSION(permission: String) =
-            "You don't have permission <$permission>"
+            "$this/commands".apply {
+                RemoveCommandStrings.load(this)
+                InviteCommandStrings.load(this)
+            }
+        }
+    }
 }
