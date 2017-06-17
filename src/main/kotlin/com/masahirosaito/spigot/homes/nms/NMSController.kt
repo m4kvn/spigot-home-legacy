5e2d6ace59f6fc2e5df8ebf7a8ebd1bc2596c540
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-06-17 18:57:01 +0900

    Create v1_12_R1 Entities

--- src/main/kotlin/com/masahirosaito/spigot/homes/nms/NMSController.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/nms/NMSController.kt
@@ -3,6 +3,7 @@ package com.masahirosaito.spigot.homes.nms
 import com.masahirosaito.spigot.homes.Homes.Companion.homes
 import com.masahirosaito.spigot.homes.nms.v1_10_R1.NMSController_v1_10_R1
 import com.masahirosaito.spigot.homes.nms.v1_11_R1.NMSController_v1_11_R1
+import com.masahirosaito.spigot.homes.nms.v1_12_R1.NMSController_v1_12_R1
 
 interface NMSController {
 
@@ -15,6 +16,7 @@ interface NMSController {
             return when (homes.server.bukkitVersion) {
                 "1.10-R0.1-SNAPSHOT", "1.10.2-R0.1-SNAPSHOT" -> NMSController_v1_10_R1()
                 "1.11-R0.1-SNAPSHOT", "1.11.2-R0.1-SNAPSHOT" -> NMSController_v1_11_R1()
+                "1.12-R0.1-SNAPSHOT", "1.12.2-R0.1-SNAPSHOT" -> NMSController_v1_12_R1()
                 else -> DummyNMSController()
             }
         }
