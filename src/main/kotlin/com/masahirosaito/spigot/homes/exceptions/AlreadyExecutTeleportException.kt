dbb4bbba7f682a094f60a9c4b7fabab251b81a61
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-06-20 16:03:58 +0900

    Create AlreadyExecuteTeleport Exception

---
+++ src/main/kotlin/com/masahirosaito/spigot/homes/exceptions/AlreadyExecutTeleportException.kt
@@ -0,0 +1,5 @@
+package com.masahirosaito.spigot.homes.exceptions
+
+import com.masahirosaito.spigot.homes.strings.ErrorStrings
+
+class AlreadyExecutTeleportException : HomesException(ErrorStrings.ALREADY_EXECUTE_TELEPORT())
