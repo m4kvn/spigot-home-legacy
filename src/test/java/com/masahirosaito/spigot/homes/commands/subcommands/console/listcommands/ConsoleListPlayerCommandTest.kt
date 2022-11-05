206b8baf4ed4083d2565ce6ad1ca306835386133
m4kvn <fun.masahiro@gmail.com>
2022-11-06 01:22:32 +0900

    Fix ConsoleCommandTests

--- src/test/java/com/masahirosaito/spigot/homes/commands/subcommands/console/listcommands/ConsoleListPlayerCommandTest.kt
+++ src/test/java/com/masahirosaito/spigot/homes/commands/subcommands/console/listcommands/ConsoleListPlayerCommandTest.kt
@@ -1,6 +1,5 @@
 package com.masahirosaito.spigot.homes.commands.subcommands.console.listcommands
 
-import com.masahirosaito.spigot.homes.Homes
 import com.masahirosaito.spigot.homes.PlayerDataManager
 import com.masahirosaito.spigot.homes.strings.ErrorStrings.ARGUMENT_INCORRECT
 import com.masahirosaito.spigot.homes.strings.ErrorStrings.NO_HOME
@@ -9,40 +8,20 @@ import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.homeConsoleC
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.nepian
 import com.masahirosaito.spigot.homes.testutils.executeHomeCommand
 import com.masahirosaito.spigot.homes.testutils.lastMsg
-import org.bukkit.Bukkit
-import org.bukkit.Location
-import org.bukkit.Server
-import org.bukkit.World
-import org.bukkit.command.PluginCommand
-import org.bukkit.entity.Player
-import org.bukkit.plugin.PluginDescriptionFile
-import org.bukkit.plugin.PluginManager
-import org.bukkit.plugin.RegisteredServiceProvider
-import org.bukkit.plugin.ServicesManager
-import org.bukkit.plugin.java.JavaPluginLoader
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.not
-import org.junit.After
-import org.junit.Assert.assertThat
-import org.junit.Assert.assertTrue
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.powermock.core.classloader.annotations.PrepareForTest
-import org.powermock.modules.junit4.PowerMockRunner
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
-@RunWith(PowerMockRunner::class)
-@PrepareForTest(Homes::class, JavaPluginLoader::class, PluginDescriptionFile::class,
-        Server::class, PluginCommand::class, Player::class, Location::class, World::class, Bukkit::class,
-        PluginManager::class, ServicesManager::class, RegisteredServiceProvider::class)
+@Suppress("NonAsciiCharacters", "TestFunctionName")
 class ConsoleListPlayerCommandTest {
 
-    @Before
+    @BeforeEach
     fun setUp() {
         assertTrue(TestInstanceCreator.setUp())
     }
 
-    @After
+    @AfterEach
     fun tearDown() {
         assertTrue(TestInstanceCreator.tearDown())
     }
@@ -50,8 +29,7 @@ class ConsoleListPlayerCommandTest {
     @Test
     fun コンソールから実行したコマンドの引数が間違っていた場合に使い方を表示する() {
         homeConsoleCommandSender.executeHomeCommand("list", "Nepian", "Minene")
-        assertThat(homeConsoleCommandSender.lastMsg(),
-                `is`(ARGUMENT_INCORRECT(ConsoleListCommand().usage.toString())))
+        assertEquals(homeConsoleCommandSender.lastMsg(), ARGUMENT_INCORRECT(ConsoleListCommand().usage.toString()))
     }
 
     @Test
@@ -61,6 +39,6 @@ class ConsoleListPlayerCommandTest {
             setNamedHomePrivate(nepian, "home1", true)
         }
         homeConsoleCommandSender.executeHomeCommand("list", "Nepian")
-        assertThat(homeConsoleCommandSender.lastMsg(), `is`(not(NO_HOME(nepian.name))))
+        assertNotEquals(homeConsoleCommandSender.lastMsg(), NO_HOME(nepian.name))
     }
 }
