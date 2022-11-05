b7eb6ac0437a1ba260ef99d21f12d79b1d06a1ca
m4kvn <fun.masahiro@gmail.com>
2022-11-06 01:14:52 +0900

    Fix FeeDataTest

--- src/test/java/com/masahirosaito/spigot/homes/datas/FeeDataTest.kt
+++ src/test/java/com/masahirosaito/spigot/homes/datas/FeeDataTest.kt
@@ -4,17 +4,17 @@ import com.masahirosaito.spigot.homes.load
 import com.masahirosaito.spigot.homes.loadData
 import com.masahirosaito.spigot.homes.saveData
 import com.masahirosaito.spigot.homes.testutils.TestInstanceCreator.feeFile
-import org.hamcrest.CoreMatchers.`is`
-import org.junit.Assert.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
+@Suppress("NonAsciiCharacters", "TestFunctionName")
 class FeeDataTest {
 
     @Test
     fun 設定ファイルを読みこむことができる() {
         feeFile.load()
         saveData(feeFile, FeeData(HOME = 30.0))
-        assertThat(loadData(feeFile, FeeData::class.java).HOME, `is`(30.0))
+        assertEquals(loadData(feeFile, FeeData::class.java).HOME, 30.0, 0.0)
         feeFile.delete()
     }
 }
