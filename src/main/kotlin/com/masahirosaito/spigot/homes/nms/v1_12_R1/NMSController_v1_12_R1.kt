a6ef3416470308ef5e28f50731a4cc89ac97580b
m4kvn <fun.masahiro@gmail.com>
2022-11-07 00:32:08 +0900

    Remove old NMS

--- src/main/kotlin/com/masahirosaito/spigot/homes/nms/v1_12_R1/NMSController_v1_12_R1.kt
+++ src/main/kotlin/com/masahirosaito/spigot/homes/nms/v1_12_R1/NMSController_v1_12_R1.kt
@@ -1,37 +1,29 @@
 package com.masahirosaito.spigot.homes.nms.v1_12_R1
 
-import com.masahirosaito.spigot.homes.nms.HomesEntity
-import com.masahirosaito.spigot.homes.nms.NMSController
-import com.masahirosaito.spigot.homes.nms.NMSEntityArmorStand
-import com.masahirosaito.spigot.homes.strings.HomeDisplayStrings.createDefaultHomeDisplayName
-import com.masahirosaito.spigot.homes.strings.HomeDisplayStrings.createNamedHomeDisplayName
-import org.bukkit.craftbukkit.v1_12_R1.CraftWorld
-import org.bukkit.event.entity.CreatureSpawnEvent
-
-class NMSController_v1_12_R1 : NMSController {
-
-    override fun setUp() {
-        CustomEntities.registerEntities()
-    }
-
-    override fun spawn(homesEntity: HomesEntity): List<NMSEntityArmorStand> {
-        val texts = if (homesEntity.homeName == null) {
-            createDefaultHomeDisplayName(homesEntity.offlinePlayer.name).split("\n")
-        } else {
-            createNamedHomeDisplayName(homesEntity.offlinePlayer.name, homesEntity.homeName!!).split("\n")
-        }
-        val list: MutableList<NMSEntityArmorStand> = mutableListOf()
-        val location = homesEntity.location
-
-        texts.forEachIndexed { index, text ->
-            list.add(EntityNMSArmorStand((location.world as CraftWorld).handle).apply {
-                setNMSName(text)
-                setNameVisible(!homesEntity.isPrivate)
-                setPosition(location.x, location.y + 0.8 - (index * 0.2), location.z)
-                (homesEntity.location.world as CraftWorld).handle
-                        .addEntity(this, CreatureSpawnEvent.SpawnReason.CUSTOM)
-            })
-        }
-        return list
-    }
-}
+//class NMSController_v1_12_R1 : NMSController {
+//
+//    override fun setUp() {
+//        CustomEntities.registerEntities()
+//    }
+//
+//    override fun spawn(homesEntity: HomesEntity): List<NMSEntityArmorStand> {
+//        val texts = if (homesEntity.homeName == null) {
+//            createDefaultHomeDisplayName(homesEntity.offlinePlayer.name).split("\n")
+//        } else {
+//            createNamedHomeDisplayName(homesEntity.offlinePlayer.name, homesEntity.homeName!!).split("\n")
+//        }
+//        val list: MutableList<NMSEntityArmorStand> = mutableListOf()
+//        val location = homesEntity.location
+//
+//        texts.forEachIndexed { index, text ->
+//            list.add(EntityNMSArmorStand((location.world as CraftWorld).handle).apply {
+//                setNMSName(text)
+//                setNameVisible(!homesEntity.isPrivate)
+//                setPosition(location.x, location.y + 0.8 - (index * 0.2), location.z)
+//                (homesEntity.location.world as CraftWorld).handle
+//                        .addEntity(this, CreatureSpawnEvent.SpawnReason.CUSTOM)
+//            })
+//        }
+//        return list
+//    }
+//}
